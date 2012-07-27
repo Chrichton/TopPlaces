40be5eae0f98755b2e6456495c45f948fb39c190
Heiko Goes <heiko.goes@gaebler-goes.de>
2012-07-27 18:31:46 +0200

    Places

---
+++ TopPlaces/FlickrAPIKey.h
@@ -0,0 +1,11 @@
+//
+//  FlickrAPIKey.h
+//
+//  Created for Stanford CS193p Fall 2011.
+//  Copyright 2011 Stanford University. All rights reserved.
+//
+//  Get your own key!
+//  No Flickr fetches will work without the API Key!
+//
+
+#define FlickrAPIKey @"915a58eb470563f8cde0789e586d92d0"
---
+++ TopPlaces/PlacesTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  PlacesTableViewController.h
+//  TopPlaces
+//
+//  Created by Heiko Goes on 27.07.12.
+//  Copyright (c) 2012 Heiko Goes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlacesTableViewController : UITableViewController
+
+@end
